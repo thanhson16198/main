56cd0ff6b3e02e24aaec192be436447dbdc9b82a
Hu Yuanrong <e0014513@u.nus.edu>
2017-10-03 18:13:55 +0800

    XmlAddressBookStorage.java: Edited to use the filePath variable.

--- src/main/java/seedu/address/storage/XmlAddressBookStorage.java
+++ src/main/java/seedu/address/storage/XmlAddressBookStorage.java
@@ -76,7 +76,7 @@ public class XmlAddressBookStorage implements AddressBookStorage {
 
     @Override
     public void backupAddressBook(ReadOnlyAddressBook addressBook) throws IOException {
-
+        saveAddressBook(addressBook, filePath + "-backup.xml");
     }
 
 }
