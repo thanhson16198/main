59c1e4d4a2e0ee1a533dbb2625a5afafae3fee05
Zhi Xuan Chng <e0032135@u.nus.edu>
2017-10-18 23:33:50 +0800

    Update Dev Guide with implementation of bookmark and clear_bookmark

--- src/main/java/seedu/address/logic/parser/ClearBookmarkCommandParser.java
+++ src/main/java/seedu/address/logic/parser/ClearBookmarkCommandParser.java
@@ -2,17 +2,14 @@ package seedu.address.logic.parser;
 
 import static seedu.address.commons.core.Messages.MESSAGE_INVALID_COMMAND_FORMAT;
 
-import seedu.address.commons.exceptions.IllegalValueException;
 import seedu.address.logic.commands.ClearBookmarkCommand;
 import seedu.address.logic.parser.exceptions.ParseException;
-import seedu.address.model.tag.Tag;
 
 /**
  * Parses input arguments and creates a new ClearBookmarkCommand object
  */
 public class ClearBookmarkCommandParser implements Parser<ClearBookmarkCommand> {
 
-    private final String bookmarkTag = "Bookmarked";
     private final String emptyString = "";
 
     /**
@@ -21,8 +18,6 @@ public class ClearBookmarkCommandParser implements Parser<ClearBookmarkCommand> 
      * @throws ParseException if the user provided any arguments
      */
     public ClearBookmarkCommand parse(String args) throws ParseException {
-
-        Tag toRemove;
 
         if (!args.trim().equals(emptyString)) {
             throw new ParseException(
