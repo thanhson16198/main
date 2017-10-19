275f37c2ab2133b18a4574e626ac82f59e17e8d9
Hu Yuanrong <e0014513@u.nus.edu>
2017-10-19 15:05:10 +0800

    ContactsCommandTest.java: Added JUnit Test for ContactsCommand.

---
+++ src/test/java/seedu/address/logic/commands/ContactsCommandTest.java
@@ -0,0 +1,23 @@
+package seedu.address.logic.commands;
+
+import org.junit.Rule;
+import org.junit.Test;
+import seedu.address.commons.events.ui.ShowContactsEvent;
+import seedu.address.ui.testutil.EventsCollectorRule;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static seedu.address.logic.commands.ContactsCommand.SHOWING_CONTACT_MESSAGE;
+
+public class ContactsCommandTest {
+    @Rule
+    public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
+
+    @Test
+    public void execute_help_success() {
+        CommandResult result = new ContactsCommand().execute();
+        assertEquals(SHOWING_CONTACT_MESSAGE, result.feedbackToUser);
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ShowContactsEvent);
+        assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
+    }
+}
