69cfaf10c13b1f147550f5b87ce7176bde931128
Hu Yuanrong <e0014513@u.nus.edu>
2017-10-19 14:17:42 +0800

    ContactsCommand.java: Created a new ContactsCommand.

---
+++ src/main/java/seedu/address/logic/commands/ContactsCommand.java
@@ -0,0 +1,21 @@
+package seedu.address.logic.commands;
+
+import seedu.address.commons.core.EventsCenter;
+import seedu.address.commons.events.ui.ShowContactsEvent;
+
+public class ContactsCommand extends Command {
+
+    public static final String COMMAND_WORD = "emergency";
+    public static final String COMMAND_WORD_ALIAS = "sos";
+
+    public static final String MESSAGE_USAGE = COMMAND_WORD + ": Shows useful contacts.\n"
+            + "Example: " + COMMAND_WORD;
+
+    public static final String SHOWING_CONTACT_MESSAGE = "Opened contacts window.";
+
+    @Override
+    public CommandResult execute() {
+        EventsCenter.getInstance().post(new ShowContactsEvent());
+        return new CommandResult(SHOWING_CONTACT_MESSAGE);
+    }
+}
