0a29fa5cd03a5788139ecd29ca1059a86503eb09
Hu Yuanrong <e0014513@u.nus.edu>
2017-10-19 15:18:51 +0800

    ContactsCommandTest.java: Resolved wrong import order.

--- src/test/java/seedu/address/logic/commands/ContactsCommandTest.java
+++ src/test/java/seedu/address/logic/commands/ContactsCommandTest.java
@@ -1,13 +1,14 @@
 package seedu.address.logic.commands;
 
-import org.junit.Rule;
-import org.junit.Test;
-import seedu.address.commons.events.ui.ShowContactsEvent;
-import seedu.address.ui.testutil.EventsCollectorRule;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static seedu.address.logic.commands.ContactsCommand.SHOWING_CONTACT_MESSAGE;
+
+import org.junit.Rule;
+import org.junit.Test;
+
+import seedu.address.commons.events.ui.ShowContactsEvent;
+import seedu.address.ui.testutil.EventsCollectorRule;
 
 public class ContactsCommandTest {
     @Rule
