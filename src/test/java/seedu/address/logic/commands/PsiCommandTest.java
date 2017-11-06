98d5590a28ac3ea489ae18ae0d4440c0c44a2b4f
AUNG\AungMyin <aungmyinkyawstar@gmail.com>
2017-11-06 22:27:05 +0800

    Implemented PSI Command test

--- src/test/java/seedu/address/logic/commands/PsiCommandTest.java
+++ src/test/java/seedu/address/logic/commands/PsiCommandTest.java
@@ -2,24 +2,25 @@ package seedu.address.logic.commands;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
-import static seedu.address.logic.commands.MrtCommand.SHOWING_MRT_MESSAGE;
+import static seedu.address.logic.commands.PsiCommand.SHOWING_PSI_MESSAGE;
 
 import org.junit.Rule;
 import org.junit.Test;
 
-import seedu.address.commons.events.ui.ShowMrtRequestEvent;
+import seedu.address.commons.events.ui.ShowPsiRequestEvent;
 import seedu.address.ui.testutil.EventsCollectorRule;
 
+
 //@@author aungmyin23
-public class MrtCommandTest {
+public class PsiCommandTest {
     @Rule
     public final EventsCollectorRule eventsCollectorRule = new EventsCollectorRule();
 
     @Test
-    public void execute_mrt_success() {
-        CommandResult result = new MrtCommand().execute();
-        assertEquals(SHOWING_MRT_MESSAGE, result.feedbackToUser);
-        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ShowMrtRequestEvent);
+    public void execute_psi_success() {
+        CommandResult result = new PsiCommand().execute();
+        assertEquals(SHOWING_PSI_MESSAGE, result.feedbackToUser);
+        assertTrue(eventsCollectorRule.eventsCollector.getMostRecent() instanceof ShowPsiRequestEvent);
         assertTrue(eventsCollectorRule.eventsCollector.getSize() == 1);
     }
 }
