6f05265a77d9def57ec9fc64f406d7d28ee2e510
AUNG\AungMyin <aungmyinkyawstar@gmail.com>
2017-11-06 13:38:14 +0800

    Edited Website.java

--- src/main/java/seedu/address/model/place/Website.java
+++ src/main/java/seedu/address/model/place/Website.java
@@ -4,41 +4,41 @@ import seedu.address.commons.exceptions.IllegalValueException;
 
 /**
  * Represents a Place's phone number in the address book.
- * Guarantees: immutable; is valid as declared in {@link #isValidEmail(String)}
+ * Guarantees: immutable; is valid as declared in {@link #isValidWebsite(String)}
  */
+//@@author aungmyin23
 public class Website {
 
-    public static final String MESSAGE_EMAIL_CONSTRAINTS =
-            "Place emails should be 2 alphanumeric/period strings separated by '@'";
-    public static final String EMAIL_VALIDATION_REGEX = "^[_A-Za-z0-9-\\+]+(\\.[_A-Za-z0-9-]+)*@"
-            + "[A-Za-z0-9-]+(\\.[A-Za-z0-9]+)*(\\.[A-Za-z]{2,})$";
-    public static final String EMAIL_UNKNOWN = "-";
+    public static final String MESSAGE_WEBSITE_CONSTRAINTS =
+            "Place website should contain http://www https://www.";
+    public static final String WEBSITE_VALIDATION_REGEX =
+            "https?://(www\\.)?[-a-z0-9]{2,256}\\.[a-z]{2,6}\\b([-a-zA-Z0-9@:%_+.~#?&//=]*)";
+    public static final String WEBSITE_UNKNOWN = "http://www.-.com";
 
     public final String value;
 
     /**
-     * Validates given email.
+     * Validates given website.
      *
-     * @throws IllegalValueException if given email address string is invalid.
+     * @throws IllegalValueException if given website string is invalid.
      */
-    public Email(String email) throws IllegalValueException {
-        //@@author aungmyin23
-        if (email == null) {
-            this.value = EMAIL_UNKNOWN;
+    public Website(String website) throws IllegalValueException {
+        if (website == null) {
+            this.value = WEBSITE_UNKNOWN;
         } else {
-            String trimmedEmail = email.trim();
-            if (!isValidEmail(trimmedEmail)) {
-                throw new IllegalValueException((MESSAGE_EMAIL_CONSTRAINTS));
+            String trimmedWebsite = website.trim();
+            if (!isValidWebsite(trimmedWebsite)) {
+                throw new IllegalValueException((MESSAGE_WEBSITE_CONSTRAINTS));
             }
-            this.value = trimmedEmail;
+            this.value = trimmedWebsite;
         }
     }
-
+    //@@author
     /**
      * Returns if a given string is a valid place email.
      */
-    public static boolean isValidEmail(String test) {
-        return test.matches(EMAIL_VALIDATION_REGEX) || test.equals(EMAIL_UNKNOWN);
+    public static boolean isValidWebsite(String test) {
+        return test.matches(WEBSITE_VALIDATION_REGEX) || test.equals(WEBSITE_UNKNOWN);
     }
 
     @Override
@@ -49,8 +49,8 @@ public class Website {
     @Override
     public boolean equals(Object other) {
         return other == this // short circuit if same object
-                || (other instanceof Email // instanceof handles nulls
-                && this.value.equals(((Email) other).value)); // state check
+                || (other instanceof Website // instanceof handles nulls
+                && this.value.equals(((Website) other).value)); // state check
     }
 
     @Override
