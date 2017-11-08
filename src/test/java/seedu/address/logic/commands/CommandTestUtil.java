b8bfade1aa06d3494747ed42830dcfdc4662906d
Son <thanhson16198@gmail.com>
2017-11-08 14:52:02 +0800

    Update valid websites

--- src/test/java/seedu/address/logic/commands/CommandTestUtil.java
+++ src/test/java/seedu/address/logic/commands/CommandTestUtil.java
@@ -30,8 +30,8 @@ public class CommandTestUtil {
     public static final String VALID_NAME_BOB = "Bob Choo";
     public static final String VALID_PHONE_AMY = "11111111";
     public static final String VALID_PHONE_BOB = "22222222";
-    public static final String VALID_WEBSITE_AMY = "http://www.exampleamy.com";
-    public static final String VALID_WEBSITE_BOB = "http://www.examplebob.com";
+    public static final String VALID_WEBSITE_AMY = "http://www.marinabaysands.com/";
+    public static final String VALID_WEBSITE_BOB = "http://www.wrs.com.sg/";
     public static final String VALID_ADDRESS_AMY = "Block 312, Amy Street 1";
     public static final String VALID_ADDRESS_BOB = "Block 123, Bobby Street 3";
     public static final String VALID_POSTAL_CODE_AMY = "111111";
