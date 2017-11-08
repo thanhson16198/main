7291dcc9a9592f9ec52ef3d6dae0dd88d4862902
Hu Yuanrong <e0014513@u.nus.edu>
2017-11-08 14:40:34 +0800

    Fix Storage bugs

--- src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
+++ src/test/java/seedu/address/storage/JsonUserPrefsStorageTest.java
@@ -82,7 +82,7 @@ public class JsonUserPrefsStorageTest {
     private UserPrefs getTypicalUserPrefs() {
         UserPrefs userPrefs = new UserPrefs();
         userPrefs.setGuiSettings(1000, 500, 300, 100);
-        userPrefs.setAddressBookFilePath("touristbook.xml");
+        userPrefs.setAddressBookFilePath("addressbook.xml");
         userPrefs.setAddressBookName("TypicalTouristBookName");
         return userPrefs;
     }
